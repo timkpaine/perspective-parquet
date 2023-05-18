a5c283c780f2db6e38336a6f5ab0a36822934af0
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2023-05-18 16:05:25 -0400

    bump timeouts

--- js/tests/all.spec.js
+++ js/tests/all.spec.js
@@ -8,10 +8,10 @@ async function getPsp(page, filename) {
   });
 
   // wait up to 10s for perspective to render
-  await page.waitForFunction(() => document.querySelector("perspective-viewer"), null, {timeout: 10000});
+  await page.waitForFunction(() => document.querySelector("perspective-viewer"), null, {timeout: 30000});
 
   // wait 5s for perspective to load data
-  await new Promise((resolve) => setTimeout(() => resolve(), 5000));
+  await new Promise((resolve) => setTimeout(() => resolve(), 10000));
 
   const psp = await page.evaluate(async () => {
     const viewer = document.querySelector(
